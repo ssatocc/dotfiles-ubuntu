9e8a77c8933cc99b5574eab81af85bec4165ae31
ssatocc <ssatocc@gmail.com>
2024-02-03 14:01:10 +0900

    Update setup.sh

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+FILES=(
+"bin"
+".bash_aliases"
+".bash_logout"
+".bashrc"
+".gitconfig"
+".inputrc"
+".profile"
+)
+
+# Create a symbolic link if it doesn't exist
+for f in "${FILES[@]}"; do
+    if [ ! -e "$HOME/$f" ]; then
+        ln -s "$DOTFILES_DIR/$f" "$HOME/$f"
+    fi
+done
+
+chmod +x ./bin/datedir
+chmod +x ./bin/ticktack
